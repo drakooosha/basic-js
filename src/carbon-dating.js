4ebfdf9f48d8cb03fc7c3ca5598ff308eab474c5
drakooosha <kopteloy@gmail.com>
2021-09-13 20:40:59 +0300

    feat: carbon done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if(typeof(sampleActivity) == 'string' && !isNaN(sampleActivity) && sampleActivity > 0 && sampleActivity < 15) {
+    return Math.ceil((Math.log(15/sampleActivity))/0.0001209424);
+  }
+  return false;
+  
 }
