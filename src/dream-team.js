67d162fd5563523e7dc119c21903c6386ffb8839
drakooosha <kopteloy@gmail.com>
2021-09-14 13:42:45 +0300

    feat: dream team done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let result = '';
+  let arr  = [];
+  if(!Array.isArray(members)) {
+    return false;
+  }
+    for(let i = 0; i < members.length; i++) {
+      if(typeof(members[i]) == 'string' && members[i] != '' && isNaN(members[i]) ) {
+        arr.push(members[i].trim().toLocaleUpperCase()[0]);
+      }
+    }
+  if(arr.length == 0) {
+    return false
+  }
+  else {
+    arr.sort();
+    for(let elem of arr) {
+      result += elem;
+    }
+    return result;
+  }
 }
+
+
+console.log(createDreamTeam([ ['David Abram'],
+['Robin Attfield'],
+'Thomas Berry',
+['Paul R.Ehrlich'],
+'donna Haraway',
+' BrIaN_gOodWiN  ',
+{
+  0: 'Serenella Iovino'
+},
+'Erazim Kohak',
+'  val_plumwood']));
