707172b014ad94764d69647475c1cd8095279eb3
drakooosha <kopteloy@gmail.com>
2021-09-19 23:54:01 +0300

    feat:simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,30 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${String(value)} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(typeof(position) != 'number' || this.arr[position-1] == undefined) {
+      this.arr = [];
+      throw Error("You can't remove incorrect link!");
+    }
+    else {
+      this.arr.splice(position-1,1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let arr1 = this.arr.splice(0);
+    return arr1.join('~~');
   }
 };
