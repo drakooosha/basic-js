f0fe6be7f342ec671e16b3053cc54747b76cc721
drakooosha <kopteloy@gmail.com>
2021-09-22 01:06:16 +0300

    feat:vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,64 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ export default class VigenereCipheringMachine {
+  constructor(type) {
+    if(type === false) {
+      this.type = false;
+    }
+    else {
+      this.type = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str,str1) {
+    if(str == undefined || str1 == undefined) {
+      throw Error('Incorrect arguments!');
+    }
+    else {
+      str = str.toUpperCase();
+      str1 = str1.toUpperCase();
+      while(str1.length < str.length) {
+        str1 += str1;
+      }
+      let result = '';
+      for(let i = 0; i < str.length; i++) {
+        let codeStr = str[i].charCodeAt()  - 65;
+        let codeStr1 = str1[0].charCodeAt() - 65;
+        if(0 <= codeStr && codeStr <= 25) {
+          result += String.fromCharCode(((codeStr+codeStr1) % 26)+65);
+          str1 = str1.substring(1);
+        }
+        else {
+          result += str[i];
+        }
+      }
+      return this.type === false? result.split('').reverse().join('') : result;
+    }
+  }
+  decrypt(str,str1) {
+    if(str == undefined || str1 == undefined) {
+      throw Error('Incorrect arguments!');
+    }
+    else {
+      str = str.toUpperCase();
+      str1 = str1.toUpperCase();
+      while(str1.length < str.length) {
+        str1 += str1;
+      }
+      let result = '';
+      for(let i = 0; i < str.length; i++) {
+        let codeStr = str[i].charCodeAt()  - 65;
+        let codeStr1 = str1[0].charCodeAt() - 65;
+        if(0 <= codeStr && codeStr <= 25) {
+          result += String.fromCharCode(((26-(codeStr1 - codeStr)) % 26)+65);
+          str1 = str1.substring(1);
+        }
+        else {
+          result += str[i];
+        }
+      }
+      return this.type === false? result.split('').reverse().join('') : result;
+    }
   }
 }
