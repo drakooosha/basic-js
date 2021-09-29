c2298ab1c43c11e28e9b50d14458e6ebf8e4de8c
Kirill Kopteloy <kopteloy@gmail.com>
2021-09-29 22:23:44 +0300

    feat: maxtrix-elements-sum

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -9,14 +9,26 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * matrix = [
- *  [0, 1, 1, 2],
- *  [0, 5, 0, 0],
- *  [2, 0, 3, 3]
+ *  
  * ]
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  for(let j = 0; j < matrix.length; j++){
+      for(let i = 0; i< matrix.length ; i++) {
+        if(matrix[i][j] == 0) {
+          j++;
+          if(j == matrix[0].length) {
+            return sum;
+          }
+          i = -1;
+        }
+        else {
+          sum += matrix[i][j];
+        }
+      }
+    }
+    return sum
 }
